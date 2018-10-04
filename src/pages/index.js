59eb33cddafe77d56899cd45944163e63335837b
sherzodmax <marvelhs@gmail.com>
2018-10-04 00:21:21 -0700

    ugh

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>This is my react playground for a couple of small experiments</h1>
     <p>1. Reddit API integration</p>
+    
     <p>2. Random user API integration.</p>
     <Link to="/page-2/">Page with user data</Link>
   </Layout>
