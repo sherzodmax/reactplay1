74986d6f6c2486fe8a2c16d86182cb31a5e3edf7
sherzodmax <marvelhs@gmail.com>
2018-10-28 15:42:23 -0700

    tweaks

---
+++ src/components/testprop1.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+class testprop1 extends React.Component {
+  render() {
+    return (
+      <div>
+        <testprop1 tagline="testing strings in the props" age={500} />
+      </div>
+    );
+  }
+}
+
+export default testprop1;
+
+
+
+
+// const testprop1 = () => (
+
+//   <div>
+//     <Header tagline="testing strings in the props" age={500}/>
+//   </div>
+// )
+
+// export default testprop1
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,19 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import testprop1 from '../components/testprop1'
+
+
+
 
 const IndexPage = () => (
   <Layout>
     <h1>This is my react playground for a couple of small experiments</h1>
     <p>1. Reddit API integration</p>
     
+    <p className="tagline"><span>{this.props.tagline}</span></p>
+
+
     <p>2. Random user API integration.</p>
     <Link to="/page-2/">Page with user data</Link>
   </Layout>
